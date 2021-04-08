198b6baa6d4c7c4dba53be17314ca98e22fadda6
Mykola_Kutsil <mukola.kutsil22@gmail.com>
2021-04-08 21:09:21 +0300

    added_content_channel-slider

---
+++ js/main.js
@@ -0,0 +1,44 @@
+const swiper = new Swiper('.channel-slider', {
+    // Optional parameters
+    loop: true,
+    slidesPerView: 4,
+
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.channel-button-next',
+        prevEl: '.channel-button-prev',
+    },
+
+
+});
+
+const swiper1 = new Swiper('.channel-slider1', {
+    // Optional parameters
+    loop: true,
+    slidesPerView: 2,
+
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.channel-button-next1',
+        prevEl: '.channel-button-prev1',
+    },
+
+
+});
+
+const swiper2 = new Swiper('.channel-slider2', {
+    // Optional parameters
+    loop: true,
+    slidesPerView: 4,
+
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.channel-button-next2',
+        prevEl: '.channel-button-prev2',
+    },
+
+
+});
