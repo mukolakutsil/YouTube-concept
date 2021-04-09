d76e4c7400bb5a699d30b1c71cf6278f659831f9
Mykola_Kutsil <mukola.kutsil22@gmail.com>
2021-04-09 17:00:53 +0300

    added_adaptive

--- js/main.js
+++ js/main.js
@@ -1,7 +1,25 @@
 const swiper = new Swiper('.channel-slider', {
     // Optional parameters
     loop: true,
-    slidesPerView: 4,
+    slidesPerView: 1,
+    spaceBetween: 20,
+    breakpoints: {
+        1900: {
+            slidesPerView: 6
+        },
+        1600: {
+            slidesPerView: 5
+        },
+        1300: {
+            slidesPerView: 4
+        },
+        1100: {
+            slidesPerView: 3
+        },
+        800: {
+            slidesPerView: 2
+        },
+    },
 
 
     // Navigation arrows
@@ -16,7 +34,17 @@ const swiper = new Swiper('.channel-slider', {
 const swiper1 = new Swiper('.channel-slider1', {
     // Optional parameters
     loop: true,
-    slidesPerView: 2,
+    slidesPerView: 1,
+    spaceBetween: 20,
+    breakpoints: {
+        1600: {
+            slidesPerView: 3
+        },
+        1100: {
+            slidesPerView: 2
+        },
+    },
+
 
 
     // Navigation arrows
@@ -32,6 +60,26 @@ const swiper2 = new Swiper('.channel-slider2', {
     // Optional parameters
     loop: true,
     slidesPerView: 4,
+    spaceBetween: 20,
+    breakpoints: {
+        1900: {
+            slidesPerView: 6
+        },
+        1600: {
+            slidesPerView: 5
+        },
+        1300: {
+            slidesPerView: 4
+        },
+        1100: {
+            slidesPerView: 3
+        },
+        800: {
+            slidesPerView: 2
+        },
+    },
+
+
 
 
     // Navigation arrows
@@ -40,5 +88,18 @@ const swiper2 = new Swiper('.channel-slider2', {
         prevEl: '.channel-button-prev2',
     },
 
-
 });
+
+const searchBtn = document.querySelector('.mobile-search'),
+    mobileSearch = document.querySelector('.input-group');
+
+searchBtn.addEventListener('click', () => {
+    mobileSearch.classList.toggle('is-open');
+});
+
+if (document.documentElement.scrollWidth <= 640) {
+    swiper.destroy();
+    swiper1.destroy();
+    swiper2.destroy();
+
+}
